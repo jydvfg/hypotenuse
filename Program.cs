8c053b3573359d720be8a571785ad4226f633d90
juany <kurobaa@gmail.com>
2024-06-17 13:06:47 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace hypothenuse_calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter side A: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter side B: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            //sum of the multplication of each side by itself
+
+            double c = Math.Sqrt(a * a + b * b);
+
+            Console.WriteLine("The hypotenuse is: " + c);
+
+            Console.ReadKey();  
+        }
+    }
+}
